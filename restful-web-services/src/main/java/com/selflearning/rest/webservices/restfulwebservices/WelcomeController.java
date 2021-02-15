20e46b67046f0c2834f675f5e1307c579b314fea
Rakesh Nagapuri <Rakesh.Nagapuri@planonsoftware.com>
2021-02-15 23:02:40 +0530

    messageBean and pathvariable

--- restful-web-services/src/main/java/com/selflearning/rest/webservices/restfulwebservices/WelcomeController.java
+++ restful-web-services/src/main/java/com/selflearning/rest/webservices/restfulwebservices/WelcomeController.java
@@ -1,6 +1,7 @@
 package com.selflearning.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,12 @@ public class WelcomeController {
 	public String getWelcomeMessage() {		
 		return "Hello, welcome to my World!   -Rakesh N";
 	}
-
+    @GetMapping(path = "/welcome-message-bean")
+	public WelcomeMessageBean getWelcomeMessageBean() {
+		return new WelcomeMessageBean("Hello, welcome to my world {From message Bean}    --Rakesh N");
+	}
+    @GetMapping(path = "/welcome/pathvariable/{name}")
+	public WelcomeMessageBean getWelcomeMessageBeanPath(@PathVariable String name) {
+		return new WelcomeMessageBean("Hello, welcome to my world {From path variable} --"+name);
+	}
 }
---
+++ restful-web-services/src/main/java/com/selflearning/rest/webservices/restfulwebservices/WelcomeMessageBean.java
@@ -0,0 +1,13 @@
+package com.selflearning.rest.webservices.restfulwebservices;
+
+public class WelcomeMessageBean {
+
+	private String message;
+	public WelcomeMessageBean(String aMessage) {
+		this.message = aMessage;
+	}
+	public String getMessage() {
+		return message;
+	}
+    
+}
